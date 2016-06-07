3977c71c86ccb522b6d415f32aa7e4416749f334
Morphecien <samuel.ducange@viacesi.fr>
2016-06-07 08:59:41 +0200

    Clean Maven

---
+++ WS_40_helloworldmvc/src/main/java/jpuexia/WS_40_helloworldmvc/App.java
@@ -0,0 +1,13 @@
+package jpuexia.WS_40_helloworldmvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
